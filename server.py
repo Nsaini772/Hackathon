04821e1eade3c515aeb5888905f85382465902e4
Zach <45150570+zweatshirt@users.noreply.github.com>
2025-03-08 14:23:09 -0300

    try to fix

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from google.cloud import speech
 import base64
 
 # Load API key from credit.json
-client = speech.SpeechClient.from_service_account_file("credit.json")
+client = speech.SpeechClient.from_service_account_file("/etc/secrets/credit.json")
 
 app = Flask(__name__)
 socketio = SocketIO(app)
